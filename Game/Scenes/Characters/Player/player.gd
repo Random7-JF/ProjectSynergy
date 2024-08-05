653d776e1a6c71f70cb3332bbca60fb1790c135c
Jon Fingas <jon.fingas@gmail.com>
2024-08-05 13:27:49 -0500

    player can move, jitter issues

---
+++ Game/Scenes/Characters/Player/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var speed: float = 100.0
+@export var accel: float = 10.0
+
+func _physics_process(delta):
+	var direction: Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
+	
+	velocity.x = move_toward(velocity.x, speed * direction.x, accel)
+	velocity.y = move_toward(velocity.y, speed * direction.y, accel)
+	
+	move_and_slide()
